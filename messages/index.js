362198097ff90eb95ffb7101c3c09e0742004e09
Evgeny Poberezkin <e.poberezkin@me.com>
2015-09-14 23:27:02 +0100

    cleanup

--- messages/index.js
+++ messages/index.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     propPlural: { // plural for the word property
       en: '{{? n==1 }}y{{??}}ies{{?}}',
-      hu: '{{? n==1 }}{{??}}i{{?}}',
+      hu: '{{? n!=1 }}i{{?}}',
       ru: '{{? n==1 }}е{{?? n>=2 && n<=4 }}я{{??}}ей{{?}}',
       pl: '{{? n==1 }}e{{??}}a{{?}}'
     }
@@ -39,7 +39,6 @@ module.exports = {
     // de: 'Zusätzliche Einträge nicht erlaubt',
     // fr: 'Éléments additionnels non autorisés',
     // nb: 'Tillegselementer er ikke tillatt',
-    // 'pl-PL': 'Dodatkowe elementy są niedozwolone',
     // 'pt-PT': 'Não são permitidos itens adicionais',
     // 'sv-SE': 'Extra värden är inte tillåtna',
     // 'zh-CN': '不允许多余的元素'
@@ -53,7 +52,6 @@ module.exports = {
     // de: 'Zusätzliche Attribute nicht erlaubt',
     // fr: 'Propriétés additionnelles non autorisées',
     // nb: 'Tilleggsvariabler er ikke tillatt',
-    // 'pl-PL': 'Dodatkowe pola są niedozwolone',
     // 'pt-PT': 'Não são permitidas propriedades adicionais',
     // 'sv-SE': 'Extra parametrar är inte tillåtna',
     // 'zh-CN': '不允许多余的字段'
@@ -67,7 +65,6 @@ module.exports = {
     // de: 'Daten stimmen nicht überein mit einem der Schemas von "anyOf"',
     // fr: 'La donnée ne correspond à aucun schema de "anyOf"',
     // nb: 'Data samsvarer ikke med noe skjema fra "anyOf"',
-    // 'pl-PL': 'Dane nie pasują do żadnego wzoru z sekcji "anyOf"',
     // 'pt-PT': 'Os dados não correspondem a nenhum esquema de "anyOf"',
     // 'sv-SE': 'Värdet matchar inget av schemana "anyOf"',
     // 'zh-CN': '数据不符合以下任何一个模式 ("anyOf")'
@@ -84,7 +81,6 @@ module.exports = {
     // de: 'Abhängigkeit fehlt - Schlüssel nicht vorhanden: {missing} (wegen Schlüssel: {key})',
     // fr: 'Echec de dépendance - la clé doit exister: {missing} (du à la clé: {key})',
     // nb: 'Variabelen {missing} må være definert (på grunn av følgende variabel: {key})',
-    // 'pl-PL': 'Błąd zależności - klucz musi istnieć: {missing} (wzgledem klucza: {key})',
     // 'pt-PT': 'Uma dependência falhou - tem de existir uma chave: {missing} (devido à chave: {key})',
     // 'sv-SE': 'Saknar beroende - saknad nyckel: {missing} (beroende nyckel: {key})',
     // 'zh-CN': '依赖失败 - 缺少键 {missing} (来自键: {key})'
@@ -98,7 +94,6 @@ module.exports = {
     // de: 'Keine Übereinstimmung mit der Aufzählung (enum) für: {value}',
     // fr: 'Aucune valeur correspondante (enum) pour: {value}',
     // nb: 'Ingen samsvarende enum verdi for: {value}',
-    // 'pl-PL': 'Żadna predefiniowana wartośc nie pasuje do: {value}',
     // 'pt-PT': 'Nenhuma correspondência \'enum\' para: {value}',
     // 'sv-SE': 'Otillåtet värde: {value}',
     // 'zh-CN': '{value} 不是有效的枚举类型取值'
@@ -111,7 +106,6 @@ module.exports = {
     // en: 'Format validation failed ({message})',
     // fr: 'Échec de validation du format ({message})',
     // nb: 'Formatteringen stemmer ikke ({message})',
-    // 'pl-PL': 'Błąd zgodności z formatem ({message})',
     // 'pt-PT': 'A validação do formato falhou ({message})',
     // 'sv-SE': 'Misslyckad validering ({message})',
     // 'zh-CN': '格式校验失败 ({message})'
@@ -125,7 +119,6 @@ module.exports = {
     // de: 'Wert {value} ist größer als das Maximum {maximum}',
     // fr: 'La valeur {value} est supérieure au maximum {maximum}',
     // nb: 'Verdien {value} er større enn maksimalverdi {maximum}',
-    // 'pl-PL': 'Wartość {value} jest większa niż {maximum}',
     // 'pt-PT': 'O valor {value} é maior que o máximo {maximum}',
     // 'sv-SE': 'Värdet {value} får inte vara större än {maximum}',
     // 'zh-CN': '数值 {value} is greater 大于最大值 {maximum}'
@@ -139,7 +132,6 @@ module.exports = {
     // de: 'Wert {value} ist kleiner als das Minimum {minimum}',
     // fr: 'La valeur {value} est inférieure au minimum {minimum}',
     // nb: 'Verdien {value} er mindre enn minsteverdi {minimum}',
-    // 'pl-PL': 'Wartość {value} jest mniejsza niż {minimum}',
     // 'pt-PT': 'O valor {value} é menor que o mínimo {minimum}',
     // 'sv-SE': 'Värdet {value} får inte vara mindre än {minimum}',
     // 'zh-CN': '数值 {value} 小于最小值 {minimum}'
@@ -153,7 +145,6 @@ module.exports = {
     // de: 'Array zu lang ({length}), maximum {maximum}',
     // fr: 'Le tableau est trop long ({length}), maximum {maximum}',
     // nb: 'Listen er for lang ({length} elementer), maksimalt {maximum}',
-    // 'pl-PL': 'Tablica ma za dużo elementów ({length}), maximum {maximum}',
     // 'pt-PT': 'A \'array\' é muito longa ({length}), máximo {maximum}',
     // 'sv-SE': 'Listan är för lång ({length}), ska högst vara {maximum}',
     // 'zh-CN': '数组长度太长 ({length}), 最大长度 {maximum}'
@@ -167,7 +158,6 @@ module.exports = {
     // de: 'Array zu kurz ({length}), minimum {minimum}',
     // fr: 'Le tableau est trop court ({length}), minimum {minimum}',
     // nb: 'Listen er for kort ({length} elementer), minst {minimum}',
-    // 'pl-PL': 'Tablica ma za mało elementów ({length}), minimum {minimum}',
     // 'pt-PT': 'A \'array\' é muito curta ({length}), mínimo {minimum}',
     // 'sv-SE': 'Listan är för kort ({length}), ska minst vara {minimum}',
     // 'zh-CN': '数组长度太短 ({length}), 最小长度 {minimum}'
@@ -181,7 +171,6 @@ module.exports = {
     // de: 'Zeichenkette zu lang ({length} chars), maximum {maximum}',
     // fr: 'Le texte est trop long ({length} carac.), maximum {maximum}',
     // nb: 'Strengen er for lang ({length} tegn), maksimalt {maximum}',
-    // 'pl-PL': 'Napis jest za długi ({length} )znaków, maksimum {maximum}',
     // 'pt-PT': 'A \'string\' é muito longa ({length} caracteres), máximo {maximum}',
     // 'sv-SE': 'Texten är för lång ({length} tecken), ska vara högst {maximum}',
     // 'zh-CN': '字符串太长 ({length} 个字符), 最多 {maximum} 个'
@@ -195,7 +184,6 @@ module.exports = {
     // de: 'Zeichenkette zu kurz ({length} chars), minimum {minimum}',
     // fr: 'Le texte est trop court ({length} carac.), minimum {minimum}',
     // nb: 'Strengen er for kort ({length} tegn), minst {minimum}',
-    // 'pl-PL': 'Napis jest za krótki ({length} znaków), minimum {minimum}',
     // 'pt-PT': 'A \'string\' é muito curta ({length} caracteres), mínimo {minimum}',
     // 'sv-SE': 'Texten är för kort ({length} tecken), ska vara minst {minimum} tecken',
     // 'zh-CN': '字符串太短 ({length} 个字符), 最少 {minimum} 个'
@@ -209,7 +197,6 @@ module.exports = {
     // de: 'Zu viele Attribute definiert ({propertyCount}), maximum {maximum}',
     // fr: 'Trop de propriétés définies ({propertyCount}), maximum {maximum}',
     // nb: 'For mange variabler definert ({propertyCount}), makismalt {maximum} er tillatt',
-    // 'pl-PL': 'Za dużo zdefiniowanych pól ({propertyCount}), maksimum {maximum}',
     // 'pt-PT': 'Muitas propriedades definidas ({propertyCount}), máximo {maximum}',
     // 'sv-SE': 'För många parametrar ({propertyCount}), får högst vara {maximum}',
     // 'zh-CN': '字段数过多 ({propertyCount}), 最多 {maximum} 个'
@@ -223,7 +210,6 @@ module.exports = {
     // de: 'Zu wenige Attribute definiert ({propertyCount}), minimum {minimum}',
     // fr: 'Pas assez de propriétés définies ({propertyCount}), minimum {minimum}',
     // nb: 'For få variabler definert ({propertyCount}), minst {minimum} er forventet',
-    // 'pl-PL': 'Za mało zdefiniowanych pól ({propertyCount}), minimum {minimum}',
     // 'pt-PT': 'Poucas propriedades definidas ({propertyCount}), mínimo {minimum}',
     // 'sv-SE': 'För få parametrar ({propertyCount}), ska minst vara {minimum}',
     // 'zh-CN': '字段数过少 ({propertyCount}), 最少 {minimum} 个'
@@ -237,7 +223,6 @@ module.exports = {
     // de: 'Wert {value} ist kein Vielfaches von {multipleOf}',
     // fr: 'La valeur {value} n\'est pas un multiple de {multipleOf}',
     // nb: 'Verdien {value} er ikke et multiplum av {multipleOf}',
-    // 'pl-PL': 'Wartość {value} nie jest wielokrotnością {multipleOf}',
     // 'pt-PT': 'O valor {value} não é um múltiplo de {multipleOf}',
     // 'sv-SE': 'Värdet {value} är inte en multipel av {multipleOf}',
     // 'zh-CN': '数值 {value} 不是 {multipleOf} 的倍数'
@@ -251,7 +236,6 @@ module.exports = {
     // de: 'Daten stimmen mit dem "not" Schema überein',
     // fr: 'La donnée correspond au schema de "not"',
     // nb: 'Data samsvarer med skjema fra "not"',
-    // 'pl-PL': 'Dane pasują do wzoru z sekcji "not"',
     // 'pt-PT': 'Os dados correspondem a um esquema de "not"',
     // 'sv-SE': 'Värdet matchar schemat från "not"',
     // 'zh-CN': '数据不应匹配以下模式 ("not")'
@@ -265,7 +249,6 @@ module.exports = {
     // de: 'Daten stimmen nicht überein mit einem der Schemas von "oneOf"',
     // fr: 'La donnée ne correspond à aucun schema de  "oneOf"',
     // nb: 'Data samsvarer ikke med noe skjema fra "oneOf"',
-    // 'pl-PL': 'Dane nie pasują do żadnego wzoru z sekcji "oneOf"',
     // 'pt-PT': 'Os dados não correspondem a nenhum esquema de "oneOf"',
     // 'sv-SE': 'Värdet matchar inget av schemana "oneOf"',
     // 'zh-CN': '数据不符合以下任何一个模式 ("oneOf")'
@@ -279,7 +262,6 @@ module.exports = {
     // de: 'Zeichenkette entspricht nicht dem Muster: {pattern}',
     // fr: 'Le texte ne correspond pas au motif: {pattern}',
     // nb: 'Strengen samsvarer ikke med regulært uttrykk: {pattern}',
-    // 'pl-PL': 'Napis nie pasuje do wzoru: {pattern}',
     // 'pt-PT': 'A \'string\' não corresponde ao modelo: {pattern}',
     // 'sv-SE': 'Texten har fel format: {pattern}',
     // 'zh-CN': '字符串不匹配模式: {pattern}'
@@ -293,7 +275,6 @@ module.exports = {
     // de: 'Notwendiges Attribut fehlt: {key}',
     // fr: 'Propriété requise manquante: {key}',
     // nb: 'Mangler obligatorisk variabel: {key}',
-    // 'pl-PL': 'Brakuje wymaganego pola: {key}',
     // 'pt-PT': 'Propriedade necessária em falta: {key}',
     // 'sv-SE': 'Egenskap saknas: {key}',
     // 'zh-CN': '缺少必要字段: {key}'
@@ -307,7 +288,6 @@ module.exports = {
     // de: 'Ungültiger Typ: {type} (erwartet wurde: {expected})',
     // fr: 'Type invalide: {type} ({expected} attendu)',
     // nb: 'Ugyldig type: {type} (forventet {expected})',
-    // 'pl-PL': 'Niepoprawny typ: {type} (spodziewany {expected})',
     // 'pt-PT': 'Tipo inválido: {type} (esperava {expected})',
     // 'sv-SE': 'Otillåten typ: {type} (skall vara {expected})',
     // 'zh-CN': '当前类型 {type} 不符合期望的类型 {expected}'
@@ -321,7 +301,6 @@ module.exports = {
     // de: 'Array Einträge nicht eindeutig (Index {match1} und {match2})',
     // fr: 'Des éléments du tableau ne sont pas uniques (indices {match1} et {match2})',
     // nb: 'Elementene er ikke unike (indeks {match1} og {match2} er like)',
-    // 'pl-PL': 'Elementy tablicy nie są unikalne (indeks {match1} i {match2})',
     // 'pt-PT': 'Os itens da \'array\' não são únicos (índices {match1} e {match2})',
     // 'sv-SE': 'Listvärden är inte unika (index {match1} och {match2})',
     // 'zh-CN': '数组元素不唯一 (下标 {match1} 和 {match2})'

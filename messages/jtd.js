ab0f66d8bc43451e32a1ea15346bc75685a71487
Johannes Maximilian Toball <jmtoball@gedankenacker.de>
2021-03-15 14:23:38 +0100

    Add German translations

--- messages/jtd.js
+++ messages/jtd.js
@@ -147,6 +147,7 @@ module.exports = {
     _type: true,
     tag: {
       en: 'tag "{{=e.params.tag}}" must be string',
+      de: 'der Tag "{{=e.params.tag}}" muss eine Zeichenkette sein',
       it: 'il tag "{{=e.params.tag}}" deve essere di tipo stringa',
       nl: 'tag "{{=e.params.tag}}" moet een tekenreeks zijn',
       ru: 'поле "{{=e.params.tag}}" должно быть строкой',
@@ -154,6 +155,7 @@ module.exports = {
     },
     mapping: {
       en: 'value of tag "{{=e.params.tag}}" must be in mapping',
+      de: 'der Wert vom Tag "{{=e.params.tag}}" muss im Mapping enthalten sein',
       it: 'il valore del tag "{{=e.params.tag}}" deve essere nei mapping',
       nl: 'de waarde van het veld "{{= e.params.tag}}" moet voorkomen in de mapping',
       ru: 'значение поля "{{=e.params.tag}}" должно быть ключом одной из схем',
